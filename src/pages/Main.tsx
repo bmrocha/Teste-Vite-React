2f9c35c51d8b586d57d2844de547f87417b7f93a
Bruno Martins Rocha <bmrocha7l@gmail.com>
2022-03-22 22:54:17 -0300

    inicial commit

---
+++ src/pages/Main.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+
+function Main() {
+  return (
+    <div>Olá Mundo Doido !!!</div>
+  )
+}
+
+export default Main;
---
+++ src/styles/global.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components'
+
+const globalStyle = createGlobalStyle`
+    * {
+        padding: 0;
+        margin:0;
+        box-sizing: border-box;
+    }
+
+    button {
+        back
+    }
+`
+const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: palevioletred;
+`;
+
+export default globalStyle;
