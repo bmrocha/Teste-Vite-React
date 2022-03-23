2fce2b1f5b26fa38601d924b42f3b7f032d9f2d7
Bruno Martins Rocha <bmrocha7l@gmail.com>
2022-03-22 22:58:24 -0300

    aaaaa

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -11,10 +11,5 @@ const globalStyle = createGlobalStyle`
         back
     }
 `
-const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: palevioletred;
-`;
 
 export default globalStyle;
